fd357153dd87d4e3bda8be64e5e16d174a36e5dd
Grydr <jalucahyoseno@gmail.com>
2024-10-07 11:48:01 +0700

    add foot config

--- .config/zsh/zsh-custom/path.zsh
+++ .config/zsh/zsh-custom/path.zsh
@@ -12,6 +12,9 @@ export RANGER_LOAD_DEFAULT_RC=false
 export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
 
+# to support running gazebo & rviz2 on wayland 
+export QT_QPA_PLATFORM=xcb
+
 export NVM_LAZY_LOAD=true
 export NVM_COMPLETION=true
 
