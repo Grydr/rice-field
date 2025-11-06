e10b72f4dba14e0b4186de99f8267764658c68b5
Grydr <jalucahyoseno@gmail.com>
2025-11-06 23:19:44 +0700

    Kamis, 06 November 2025

--- .config/zsh/zsh-custom/path.zsh
+++ .config/zsh/zsh-custom/path.zsh
@@ -1,17 +1,16 @@
-# export ZIM_CONFIG_FILE=$HOME/.config/zim/zimrc
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:$XDG_DATA_DIRS"
 export XDG_DATA_DIRS="/home/gryd/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_STATE_HOME=$HOME/.local/state
-export WAYLAND_DISPLAY=wayland-0
 export XDG_RUNTIME_DIR=/run/user/1000
-export EDITOR='nvim'
-# export TMUX_TMPDIR=/var/tmp
+
+export WAYLAND_DISPLAY=wayland-0
+export TMUX_TMPDIR=/var/tmp
 
 export PATH=$HOME/.config/composer/vendor/bin:$PATH
-export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.npm-packages/bin:$PATH
 
 # to support running gazebo & rviz2 on wayland
 export QT_QPA_PLATFORM=xcb
@@ -21,4 +20,4 @@ export NVM_COMPLETION=true
 
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
 
-export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+export EDITOR='nvim'
